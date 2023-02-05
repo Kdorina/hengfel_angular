d8935f4d4ad439830ae7359556e9896c72d0e642
Kovács Dorina <kovacsdorina2@gmail.com>
2023-02-05 22:54:31 +0100

    0318 feladat megkezdése / komponensek felvétele

---
+++ src/app/cylinder/cylinder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cylinder',
+  templateUrl: './cylinder.component.html',
+  styleUrls: ['./cylinder.component.scss']
+})
+export class CylinderComponent {
+
+}
