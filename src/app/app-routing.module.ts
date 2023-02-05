1a285b7e036d6bb5039c03e6f6d2249b0b654334
Kovács Dorina <kovacsdorina2@gmail.com>
2023-02-05 23:06:17 +0100

    navigációs sáv kész / routing felvéve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,15 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CylinderComponent } from './cylinder/cylinder.component';
+import { GithubComponent } from './github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cylinder", component:CylinderComponent},
+  {path:"about", component:AboutComponent},
+  {path:"github", component:GithubComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
